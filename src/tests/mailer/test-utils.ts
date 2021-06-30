19c98bfd8029d4f48381f1832b6cc8353080f36c
Henry T <henry@henrytu.me>
2021-06-30 00:38:52 -0400

    Send all templates test

--- src/tests/mailer/test-utils.ts
+++ src/tests/mailer/test-utils.ts
@@ -43,3 +43,18 @@ export const mockGetList = {
     listID: 'wtf',
   },
 };
+
+export const mockMailTemplate = {
+  template1: {
+    subject: 'Subject 1',
+    templateID: 1,
+  },
+  template2: {
+    subject: 'Subject 2',
+    templateID: 2,
+  },
+  template3: {
+    subject: 'Subject 3',
+    templateID: 3,
+  },
+};
