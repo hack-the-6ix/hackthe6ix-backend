945cf81007404909633752564c5a99d0328bbf4a
Henry T <henry@henrytu.me>
2021-07-03 22:35:33 -0400

    Updated program of study

--- src/controller/UserController.ts
+++ src/controller/UserController.ts
@@ -1,5 +1,6 @@
 import { Mongoose } from 'mongoose';
-import { enumOptions, fields, IApplication, IUser } from '../models/user/fields';
+import { enumOptions } from '../models/user/enums';
+import { fields, IApplication, IUser } from '../models/user/fields';
 import User from '../models/user/User';
 import { isConfirmationOpen } from '../models/validator';
 import sendTemplateEmail from '../services/mailer/sendTemplateEmail';
