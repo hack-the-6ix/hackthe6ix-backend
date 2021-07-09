4229edf0cb2e21adabeaa1d6fba84b9b3ae4e812
David Hui <opensource@davidhui.ca>
2021-07-08 20:35:23 -0400

    write logs to subdirectory

--- src/services/logger.ts
+++ src/services/logger.ts
@@ -52,8 +52,8 @@ function createWinstonLogger() {
       // - Write to all logs with level `info` and below to `combined.log`
       // - Write all logs error (and below) to `error.log`.
       //
-      new winston.transports.File({ filename: 'error.log' , level:'error', handleExceptions: true}),
-      new winston.transports.File({ filename: 'combined.log' })
+      new winston.transports.File({ filename: 'logs/error.log' , level:'error', handleExceptions: true}),
+      new winston.transports.File({ filename: 'logs/combined.log' })
     ]//,
     // exceptionHandlers: [
     //     new winston.transports.File({ filename: 'exceptions.log' }),
