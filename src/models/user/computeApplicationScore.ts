7a91b837d92b158ed51e416ee7daf109d23187d5
David Hui <opensource@davidhui.ca>
2022-07-16 02:54:25 -0400

    update grading for 2022

--- src/models/user/computeApplicationScore.ts
+++ src/models/user/computeApplicationScore.ts
@@ -9,7 +9,7 @@ export default function() {
   const firstHackathon = user.hackerApplication?.hackathonsAttended === enumOptions.hackathonsAttended[0];
 
   // Ensure all categories are graded before performing the calculation
-  for (const category of ['accomplish', 'project', 'portfolio']) {
+  for (const category of ['whyHT6', 'techInnovation', 'project', 'portfolio']) {
     if (
       (category !== 'portfolio' || !firstHackathon) && // If this is the applicant's first hackathon, we waive the validation for portfolio
       (applicationScores[category]?.score < 0) // Ensure we have all the relevant data on file
@@ -19,9 +19,10 @@ export default function() {
   }
 
   const total: number = (user.hackerApplication.requestedWorkshops?.length > 0 ? 1 : 0) +
-    applicationScores.accomplish?.score +
+    applicationScores.techInnovation?.score +
+    applicationScores.whyHT6?.score +
     applicationScores.project?.score +
     (!firstHackathon ? applicationScores.portfolio?.score : 0);
 
-  return (total / (firstHackathon ? 9 : 11)) * 100;
+  return (total / (firstHackathon ? 13 : 15)) * 100;
 }
